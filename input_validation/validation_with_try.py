93ba1a34952abdbb6350690d5a0d7e94683b58b6
ComputerUser158 <oneringraw@gmail.com>
2020-09-21 17:41:39 -0500

    second commit pass

--- input_validation/validation_with_try.py
+++ input_validation/validation_with_try.py
@@ -5,7 +5,9 @@ prompts 3 scores and returns average to the user
 NUMBER_TESTS = 3
 
 def average(score1, score2, score3):
-    return(score1 + score2 + score3)/NUMBER_TESTS
+    if score1 < 0 or score2 < 0 or score3 < 0:
+        raise ValueError
+    return float(score1 + score2 + score3)/NUMBER_TESTS
 
 
 if __name__ == "__main__":
