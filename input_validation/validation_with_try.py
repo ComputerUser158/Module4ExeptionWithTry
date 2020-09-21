3ab49de16db009b00a75c76c7aee88ce991201da
ComputerUser158 <oneringraw@gmail.com>
2020-09-21 17:32:29 -0500

    first commit fail

---
+++ input_validation/validation_with_try.py
@@ -0,0 +1,16 @@
+"""
+author: Rawley Collins
+prompts 3 scores and returns average to the user
+"""
+NUMBER_TESTS = 3
+
+def average(score1, score2, score3):
+    return(score1 + score2 + score3)/NUMBER_TESTS
+
+
+if __name__ == "__main__":
+    lastName = input("enter your last name: ")
+    firstName = input("enter your first name: ")
+    age = input("enter your age: ")
+    average_scores = average(10, 90, 100)
+    print("{}, {} age: {} average grade: {}".format(lastName, firstName, age, average_scores))
